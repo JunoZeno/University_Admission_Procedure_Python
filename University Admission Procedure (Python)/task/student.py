1690b3844e416395ac9efd655c78ccc555bc9f6e
JunoZeno <juliohernandez235@gmail.com>
2024-05-05 22:35:37 -0400

    Add a list of priorities for the Student class

--- University Admission Procedure (Python)/task/student.py
+++ University Admission Procedure (Python)/task/student.py
@@ -10,10 +10,11 @@ init_debug_and_info_handlers(logger)
 class Student:
     amount_scores_required = 3
 
-    def __init__(self, first_name, last_name, gpa):
+    def __init__(self, first_name, last_name, gpa, first_choice, second_choice, third_choice):
         self.first_name: str = first_name
         self.last_name: str = last_name
         self.gpa: float = gpa
+        self.priorities = [first_choice, second_choice, third_choice]
         self.scores: list[float] = []
 
     def add_scores(self):
@@ -26,7 +27,7 @@ class Student:
         return sum(self.scores) / len(self.scores)
 
     def __repr__(self):
-        return f"{self.first_name} {self.last_name} {self.gpa}"
+        return f"Student(first_name='{self.first_name}', last_name='{self.last_name}', gpa={self.gpa}, priorities={self.priorities})"
 
     def __str__(self):
         return f"{self.first_name} {self.get_average_score()}"
